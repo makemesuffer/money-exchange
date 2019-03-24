49478e2f6ce705d45ce5eab0c7151158bc6cbbe5
makemesuffer <strigaliov4working@gmail.com>
2019-03-24 19:59:36 +0300

    Wha

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency >= 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    }
+
+    const values = [{value: 50, name: 'H'},
+        {value: 25, name: 'Q'},
+        {value: 10, name: 'D'},
+        {value: 5, name: 'N'},
+        {value: 1, name: 'P'}];
+
+    let money = {};
+
+    values.forEach(function (e) {
+        let value;
+        if ((value = Math.floor(currency / e.value)) > 0) {
+            money[e.name] = value;
+            currency = Math.floor(currency % e.value);
+        }
+
+    });
+
+    return money;
+};
